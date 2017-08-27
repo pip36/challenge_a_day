837b47ffc50dc5977f0d7f596ab08a5e61190686
pip36 <phil_23@live.co.uk>
2017-08-27 22:37:10 +0100

    fix devise registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
   resources :statuses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home', to: 'static_pages#index'
